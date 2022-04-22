c261eb3cbbd76e1738176cca7d20e0f2b1663692
Mani kumar <manikumarg67@gmail.com>
2022-04-22 12:02:51 +0530

    startnginx

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user/sample-niginx
-sudo systemctl start nginx
-sudo systemctl status nginx
-#docker-compose build --no-cache
-#docker-compose up -d
+docker-compose build --no-cache
+docker-compose up -d
